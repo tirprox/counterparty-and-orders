cbf72dfc916bfc4d9f9536d813862966ad6a1993
Gleb Samsonov <samsonov.gleb@gmail.com>
2018-01-19 17:04:04 +0300

    added sms send functions

--- export-csv-counteragents.php
+++ export-csv-counteragents.php
@@ -10,11 +10,17 @@ $data = [];
 foreach ($csvFile as $line) {
     $data[] = str_getcsv($line, ";");
 }
-$i = 1;
+/*$i = 1;
 foreach ($data as $item) {
     print("iteration: $i\n");
     $exporter->exportOldAnketaItem($item);
     $i++;
     //$this->createTag($item);
     //var_dump($item);
+}*/
+
+$size = count($data);
+for ($i=916; $i<$size; $i++) {
+    print("iteration: $i\n");
+    $exporter->exportOldAnketaItem($data[$i]);
 }
