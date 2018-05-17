60e918b42f198bf6bc1d6626bb7d8e212961b417
tirprox <samsonov.gleb@gmail.com>
2018-05-17 16:18:04 +0300

    safety commit

---
+++ VisitorResolver.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gleb
+ * Date: 4/20/18
+ * Time: 3:26 PM
+ */
+require_once "Counterparty.php";
+require_once "MSAuth.php";
+
+
+class VisitorResolver
+{
+
+}
